8ae532f933f6d9edb6332f062331a23478196e7d
Ayaz Namazov <ayaznamazov019@gmail.com>
2024-02-07 23:18:22 +0300

    Feat: notification data loading operations
    The functionality of opening cont notifications with type "=handleShowContNotifications"
    has been completed, in which the following actions were performed: when opening the notification icon,
    if there are unseen notifications, then first of all, the ID is written to the object according
    to the current cycle, then the array is overwritten from action to an array with a hook, the same actions
    are performed to open the existing viewed ones notifications. Also, when opened, the notification counter is reset,
    which is used from the context hook, which counts the available number of unread notifications. Further, after closing,
    both sections are overwritten from unread notifications to viewed ones, in addition, the changed state of the object
    field "isViewedNotifcation: true" is added, new data of viewed notifications is also saved to action and to the db collection,
    in addition, action and data from the database about unread notifications are cleared using the corresponding dispatches.

--- src/components/componentsHomePage/notificationFunctionality.jsx
+++ src/components/componentsHomePage/notificationFunctionality.jsx
@@ -48,14 +48,173 @@ export default function NotificationFunctionality() {
 
     const [eventClickTracking, setEventClickTracking] = useState(1);
 
-    const handleShowContNotifications = () => {
+    const lengthArrUnseen = notificationsUnseen && notificationsUnseen.length ? notificationsUnseen.length : 0;
+
+    const lengthArrViewed = notificationsViewed && notificationsViewed.length ? notificationsViewed.length : 0;
+
+    const checkNots = lengthArrUnseen || lengthArrViewed ? true : false;
+
+
+
+    const [updateArrDataViewedNotHookData, setUpdateArrDataViewedNotHookData] = useState([]);
+    const [updateArrDataUnseenNotHookData, setUpdateArrDataUnseenNotHookData] = useState([]);
+    const [isLoadingDataNots, setLoadingDataNots] = useState(false);
+
+    const handleShowContNotifications = async () => {
+        setLoadingDataNots(true);
         setHideNotificationIcon(false);
-        setEventClickTracking(eventClickTracking + 1);
-        if(!hideNotificationIcon){
-            if(eventClickTracking % 2 == 0){
-                setHideNotificationIcon(true);
+        const updatedArrDataViewedNot = [];
+        const updatedArrDataUnseenNot = [];
+        if (lengthArrViewed > 0) {
+            for (let i = 0; i < lengthArrViewed; i++) {
+                const idObj = i + 0;
+                const textNotDataObjectArr = notificationsViewed[i]?.payload[0]?.textNotification;
+                const dateNotDataObjectArr = notificationsViewed[i]?.payload[0]?.dateNotification;
+                const categNotDataObjecetArr = notificationsViewed[i]?.payload[0]?.categoryNotification;
+                const senderNotDataOgjectArr = notificationsViewed[i]?.payload[0]?.senderNotification;
+                const isHideNot = notificationsViewed[i]?.payload[0]?.isHideNot;
+                const isBannedNot = notificationsViewed[i]?.payload[0]?.isBannedNot;
+
+                const addingObjArrViewedNot = {
+                    id: idObj,
+                    textNotification: textNotDataObjectArr,
+                    dateNotification: dateNotDataObjectArr,
+                    categoryNotification: categNotDataObjecetArr,
+                    senderNotification: senderNotDataOgjectArr,
+                    isHideNot: isHideNot,
+                    isBannedNot: isBannedNot
+                };
+                updatedArrDataViewedNot.push(addingObjArrViewedNot);
             }
-            else{
+            setUpdateArrDataViewedNotHookData(updatedArrDataViewedNot);
+        }
+        if (lengthArrUnseen > 0) {
+            for (let i = 0; i < lengthArrUnseen; i++) {
+                const id = i + 0;
+                const textNotDataObjectArr = notificationsUnseen[i]?.payload[0]?.textNotification;
+                const dateNotDataObjectArr = notificationsUnseen[i]?.payload[0]?.dateNotification;
+                const categNotDataObjecetArr = notificationsUnseen[i]?.payload[0]?.categoryNotification;
+                const senderNotDataOgjectArr = notificationsUnseen[i]?.payload[0]?.senderNotification;
+                const isHideNot = notificationsUnseen[i]?.payload[0]?.isHideNot;
+                const isBannedNot = notificationsUnseen[i]?.payload[0]?.isBannedNot;
+
+                const addingObjArrUnseenNot = {
+                    id: id,
+                    textNotification: textNotDataObjectArr,
+                    dateNotification: dateNotDataObjectArr,
+                    categoryNotification: categNotDataObjecetArr,
+                    senderNotification: senderNotDataOgjectArr,
+                    isHideNot: isHideNot,
+                    isBannedNot: isBannedNot
+                };
+                updatedArrDataUnseenNot.push(addingObjArrUnseenNot);
+            }
+            setUpdateArrDataUnseenNotHookData(updatedArrDataUnseenNot);
+        }
+        setCalculateCountUnseenNotifications(0);
+        setEventClickTracking(eventClickTracking + 1);
+        setLoadingDataNots(false);
+        if (!hideNotificationIcon) {
+            if (eventClickTracking % 2 == 0) {
+                setCalculateCountUnseenNotifications(0);
+                setHideNotificationIcon(true);
+                if (lengthArrUnseen > 0) {
+                    const userNotificationRef = doc(db, 'usersNotifications', email);
+                    const userNotificationDocSnapshots = await getDoc(userNotificationRef);
+                    const dataUserNotifications = userNotificationDocSnapshots.data();
+                    const arrayUserNotifcationsViewedDb = dataUserNotifications && dataUserNotifications.arrayUserNotifcationsViewed ? true : false;
+
+
+                    for (let i = 0; i < lengthArrUnseen; i++) {
+                        const objectArrDateTimeReceivingNotification = notificationsUnseen[i]?.payload[0]?.dateNotification;
+                        const objectArrTextNotification = notificationsUnseen[i]?.payload[0]?.textNotification;
+                        const objectArrSenderNotification = notificationsUnseen[i]?.payload[0]?.senderNotification;
+                        const categNotDataObjecetArr = notificationsUnseen[i]?.payload[0]?.categoryNotification;
+                        const isHideNot = notificationsUnseen[i]?.payload[0]?.isHideNot;
+                        const isBannedNot = notificationsUnseen[i]?.payload[0]?.isBannedNot;
+
+                        //dispatching and adding data db viewed notification
+                        dispatch(setOperationUserNotifications({
+                            type: 'ADD_NOTIFICATIONS_VIEWED',
+                            payload: [
+                                {
+                                    isViewedNotifcation: true,
+                                    dateNotification: objectArrDateTimeReceivingNotification,
+                                    textNotification: objectArrTextNotification,
+                                    senderNotification: objectArrSenderNotification,
+                                    categoryNotification: categNotDataObjecetArr,
+                                    isHideNot: isHideNot,
+                                    isBannedNot: isBannedNot
+                                }
+                            ]
+                        }));
+
+                        const dataObjViewedArrNotificaion = {
+                            isViewedNotifcation: true,
+                            dateNotification: objectArrDateTimeReceivingNotification,
+                            textNotification: objectArrTextNotification,
+                            senderNotification: objectArrSenderNotification,
+                            categoryNotification: categNotDataObjecetArr,
+                            isHideNot: isHideNot,
+                            isBannedNot: isBannedNot
+                        }
+
+                        if (!arrayUserNotifcationsViewedDb) {
+                            const fieldName = 'arrayUserNotifcationsViewed';
+
+                            const addArrayNotificationsViewedData = [dataObjViewedArrNotificaion];
+
+                            const updateNotificationsField = async (email, fieldName, addArrayNotificationsViewedData) => {
+                                const notificationsRef = doc(db, 'usersNotifications', email);
+                                try {
+                                    await updateDoc(notificationsRef, { [fieldName]: arrayUnion(...addArrayNotificationsViewedData) });
+                                }
+                                catch (error) {
+                                    console.log(error);
+                                }
+                            };
+                            await updateNotificationsField(email, fieldName, addArrayNotificationsViewedData);
+                        }
+                        else {
+                            const field = 'arrayUserNotifcationsViewed';
+                            const value = [];
+                            value.push(dataObjViewedArrNotificaion);
+
+                            const userNotificationRef = doc(db, 'usersNotifications', email);
+                            const setDataField = { [field]: value };
+
+                            try {
+                                await setDoc(userNotificationRef, setDataField, { merge: true })
+                            }
+                            catch (error) {
+                                console.log(error);
+                            }
+                        }
+                    }
+                    //deleting data db unseen notifications and
+                    //dispatching empty unseen notificatons array in action
+                    const fieldName = 'arrayUserNotifcationsUnseen';
+
+                    const deletingObjectsElsArrayNotificiatosUnseen = [];
+
+                    const updateNotificationsField = async (email, fieldName, deletingObjectsElsArrayNotificiatosUnseen) => {
+                        const notificationsRef = doc(db, 'usersNotifications', email);
+                        try {
+                            await updateDoc(notificationsRef, { [fieldName]: deletingObjectsElsArrayNotificiatosUnseen });
+                        }
+                        catch (error) {
+                            console.log(error);
+                        }
+                    };
+                    await updateNotificationsField(email, fieldName, deletingObjectsElsArrayNotificiatosUnseen);
+
+                    dispatch(setOperationUserNotifications({ type: 'REMOVE_UNSEEN_NOTIFICATIONS' }));
+
+                    setUpdateArrDataViewedNotHookData([]);
+                    setUpdateArrDataUnseenNotHookData([]);
+                }
+            }
+            else {
                 setHideNotificationIcon(false);
             }
         }
