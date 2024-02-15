7a8cdad8daecb3c5fa4a9704b48b99d42cf459f1
Ayaz Namazov <ayaznamazov019@gmail.com>
2024-02-15 22:50:27 +0300

    Feat: add action arrays - "notificationsBanned", "notificationsHide"

--- src/components/globalHooks/useauth.js
+++ src/components/globalHooks/useauth.js
@@ -3,16 +3,14 @@ import { useSelector } from "react-redux";
 export function useAuth() {
     const { id, displayName, password, token,
         email, photoAcc, dateBirth, phoneNumber, townLocation, streetLocation, gender, 
-        errorsInformation,  idInformErrors,
-        fixedInformErrors, idInformErrorsFixed, isFixed, textFeedBack,
-        notificationsHistory, notificationsUnseen, notificationsViewed} = useSelector(state => state.user);
+        errorsInformation,  idInformErrors, fixedInformErrors, idInformErrorsFixed, isFixed, textFeedBack,
+        notificationsHistory, notificationsUnseen, notificationsViewed, notificationsBanned, notificationsHide} = useSelector(state => state.user);
 
     return{
         isAuth: !!email,
         id, token, displayName, password,
-        email, photoAcc, dateBirth,phoneNumber, townLocation, streetLocation, gender,
-        errorsInformation, idInformErrors,
-        fixedInformErrors, idInformErrorsFixed, isFixed, textFeedBack,
-        notificationsHistory, notificationsUnseen, notificationsViewed
+        email, photoAcc, dateBirth, phoneNumber, townLocation, streetLocation, gender,
+        errorsInformation, idInformErrors, fixedInformErrors, idInformErrorsFixed, isFixed, textFeedBack,
+        notificationsHistory, notificationsUnseen, notificationsViewed, notificationsBanned, notificationsHide
     };
 }
