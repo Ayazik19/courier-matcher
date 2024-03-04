4b39b1acbdcd6d5f59df700f777f82ee450e962d
Ayaz Namazov <ayaznamazov019@gmail.com>
2024-03-04 20:51:44 +0300

    Feat: added slice variable - "hiddenNotifications" in hook

--- src/components/globalHooks/useNotificationSettings.js
+++ src/components/globalHooks/useNotificationSettings.js
@@ -3,11 +3,13 @@ import { useSelector } from "react-redux";
 export function useNotificationSettings(){
     const {
         notifications, notificationsSs, 
-        multipleNotifications, notificationsAdmin, notificationsCouriers
+        multipleNotifications, notificationsAdmin, notificationsCouriers,
+        hiddenNotifications
     } = useSelector(state => state.notificationsAgreement);
 
     return{
         notifications, notificationsSs, 
-        multipleNotifications, notificationsAdmin, notificationsCouriers
+        multipleNotifications, notificationsAdmin, notificationsCouriers,
+        hiddenNotifications
     };
 }
